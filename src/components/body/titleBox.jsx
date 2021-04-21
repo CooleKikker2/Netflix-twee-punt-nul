fd86dff605db1dee58de615af32cd832d5d41453
CooleKikker <pctcoen@gmail.com>
2021-04-21 20:56:50 +0200

    Update 21-4

--- src/components/body/titleBox.jsx
+++ src/components/body/titleBox.jsx
@@ -10,46 +10,83 @@ class titleBox extends React.Component {
         super();
         this.getMovies();
     }
+
+
     
 
-
-
-    getMovies() {
-        const baseURL = 'https://api.themoviedb.org/3/';
-        // let configData = null;
-        // let baseImageURL = null;
+    getMovies(genre = null) {
+        console.log("deadloop!")
         const APIKEY = '927b2722076db51f3b66f95e3b0419b6';
+        if(!genre){
+            
+            const baseURL = 'https://api.themoviedb.org/3/';
+            // let configData = null;
+            // let baseImageURL = null;
 
-        let completeURL = "".concat(baseURL, 'movie/popular?api_key=', APIKEY);
-        fetch(completeURL)
-            .then((result) => {
-                return result.json();
-            })
-            .then((data) => {
-                this.setState({movies: data.results});
-            })
-            .catch(function(error) {
-                console.log(error);
-            });
+            let completeURL = "".concat(baseURL, 'movie/popular?api_key=', APIKEY);
+            fetch(completeURL)
+                .then((result) => {
+                    return result.json();
+                })
+                .then((data) => {
+                    this.setState({movies: []})
+                    this.setState({movies: data.results});
+                    return null
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+            }else{
+                const baseURL = 'https://api.themoviedb.org/3/';
+                // let configData = null;
+                // let baseImageURL = null;
+                
+
+                let completeURL = "".concat(baseURL, 'discover/movie?api_key=', APIKEY + '&with_genres=' + genre);
+                return fetch(completeURL)
+                    .then((result) => {
+                        return result.json();
+                    })
+                    .then((data) => {
+                        this.setState({movies: []})
+                        this.setState({movies: data.results});
+                        return null
+                    })
+                    .catch(function(error) {
+                        console.log(error);
+                    });
+            }
+        
     }
 
+    getGenreMovies(genre){
+        let genreMovies = [];
+            this.getMovies(genre).then((gettedMovies) => {
+                gettedMovies.map((movie, index) =>{
+                    genreMovies.push(Object.assign({}, movie));
+            })
+        })
+        return genreMovies;
+    }
+
+
+
     getBoxes = (movies) => {
-        console.log(movies)
-        return(
-            movies.map((movie, index) => {
-                console.log(movie)
-                console.log("A movie with id "+ index +" has been mapped: " + movie);
-                return (
-                    <div key={index} className="titleBox">
-                        <div className="text_bg">
-                            <p>{movie.original_title}</p>
+            movies = this.state.movies
+            return(
+                
+                movies.map((movie, index) => {
+                    return (
+                        <div key={index} className="titleBox">
+                            <img src={'https://image.tmdb.org/t/p/w185' + movie.backdrop_path} alt={movie.original_title}></img>
+                            <div className="text_bg">
+                                <p>{movie.original_title}</p>
+                            </div>
                         </div>
-                        <img src={'https://image.tmdb.org/t/p/w185' + movie.backdrop_path} alt={movie.original_title}></img>
-                        
-                    </div>
-                );
-            }) 
-        )
+                    );
+                }) 
+            )
+        
     }
 
 
@@ -62,12 +99,11 @@ class titleBox extends React.Component {
             <h4>Populairste Films</h4><br />
             <div className="titles">
                 
-                <div>{this.getBoxes(this.state.movies)}</div> 
+                <div>{this.getBoxes(this.getMovies())}</div> 
             </div>
-            <h4>Ons Favorieten</h4><br />
+            <h4>Horror:</h4><br />
             <div className="titles">
-                
-                <div>{this.getBoxes(this.state.movies)}</div> 
+                <div>{this.getBoxes(this.getMovies(27), 27)}</div> 
             </div>
             </>
         )
